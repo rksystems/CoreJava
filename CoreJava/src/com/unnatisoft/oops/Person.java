220addd4223e9baa2c8e0934cb5ec2db0e12c062
raghukishore <r.raghukishore@gmail.com>
2017-06-08 13:10:31 +0530

    First

---
+++ CoreJava/src/com/unnatisoft/oops/Person.java
@@ -0,0 +1,41 @@
+package com.unnatisoft.oops;
+
+public class Person {
+	private String firstName;
+	private String lastName;
+	private String age;
+	private String gender;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+}

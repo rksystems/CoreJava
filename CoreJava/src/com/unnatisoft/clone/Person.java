ec419acdb3280e70312d84ecff28ea7ef4d67a30
raghukishore <r.raghukishore@gmail.com>
2017-06-21 16:49:36 +0530

    Added address object

--- CoreJava/src/com/unnatisoft/clone/Person.java
+++ CoreJava/src/com/unnatisoft/clone/Person.java
@@ -8,14 +8,19 @@ public class Person implements Cloneable {
 	public static void main(String[] args) throws CloneNotSupportedException {
 
 		Person p1 = new Person();
-		Person p2 = p1;
-		//Person p2 = (Person) p1.clone();
-		if(p1.equals(p2)){
+		Address a1 = new Address();
+		p1.address = a1;
+		//Person p2 = p1;
+		Person p2 = (Person) p1.clone();
+		if (p1.equals(p2)) {
 			System.out.println("Objects are equal");
 		}
-		
+
 		System.out.println(p1.hashCode());
 		System.out.println(p2.hashCode());
-		
+
+		System.out.println(p1.address.hashCode());
+		System.out.println(p2.address.hashCode());
+
 	}
 }

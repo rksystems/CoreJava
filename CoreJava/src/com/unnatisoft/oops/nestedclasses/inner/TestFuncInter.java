eae614069ba21743ba902c7a36c71e7861878ae5
unnati <unnati@DESKTOP-PJEIIJR>
2017-06-28 20:10:51 +0530

    FI

---
+++ CoreJava/src/com/unnatisoft/oops/nestedclasses/inner/TestFuncInter.java
@@ -0,0 +1,12 @@
+package com.unnatisoft.oops.nestedclasses.inner;
+
+public class TestFuncInter {
+	
+	public static final double PI = 3.14;
+	
+
+	public static void main(String[] args) {
+		System.out.println(TestFuncInter.PI);
+	}
+
+}
